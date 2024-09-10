e195876c5296760d92c58d7334e0f618daae5695
pecastoyolex <pecastoyolex@gmail.com>
2024-09-10 15:57:58 -0300

    Update getFilial.js

--- functions/getFilial.js
+++ functions/getFilial.js
@@ -1,24 +1,30 @@
 function getFilial(filial) {
 
- if (filial === 'RECIFE') {
+ if (filial === 'Toyolex Imbiribeira') {
   return "A"
- } else if (filial === 'OLINDA') {
-  return "E"
- } else if (filial === 'PIEDADE') {
-  return "I"
- } else if (filial === 'JOÃO PESSOA') {
-  return "D"
- } else if (filial === 'RETIRO') {
+ } else if (filial === 'Toyolex Rui Barbosa') {
   return "B"
- } else if (filial === 'FEIRA DE SANTANA') {
-  return "G"
- } else if (filial === 'HOLANDESES') {
+ } else if (filial === 'Toyolex Caruaru') {
   return "C"
- } else if (filial === 'MANAUS') {
+ } else if (filial === 'Toyolex Petrolina') {
+  return "D"
+ } else if (filial === 'Toyolex São Luís') {
+  return "E"
+ } else if (filial === 'Toyolex Tirirical') {
   return "F"
- } else if (filial === 'BEQUIMÃO') {
+ } else if (filial === 'Toyolex Manaus') {
+  return "G"
+ } else if (filial === 'Toyolex Mossoró') {
   return "H"
- } else return
+ } else if (filial === 'Toyolex Caicó') {
+  return "I"
+ } else if(filial === 'Toyolex Natal'){
+  return "J"
+ }else if(filial === 'Toyolex Boa Vista'){
+  return "K"
+ }else if(filial === 'Toyolex Aracaju'){
+    return "L"
+ }else return 
 
 }
 
